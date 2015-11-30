0bed9c0eed6d8724d2157410c1773fd7d91cc277
sweldon <stmydchannel@gmail.com>
2015-11-30 18:44:09 -0500

    Auto GPS fully works, almost manual entry

--- MapProject2/app/src/main/java/com/example/sweldon1/mapproject/Home.java
+++ MapProject2/app/src/main/java/com/example/sweldon1/mapproject/Home.java
@@ -1,6 +1,7 @@
 package com.example.sweldon1.mapproject;
 
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
@@ -8,9 +9,11 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -39,24 +42,21 @@ import java.util.ArrayList;
 public class Home extends ActionBarActivity  implements OnMapReadyCallback {
 
 
+    private static final String TAG = "";
     private TextView location;
     private TextView body;
     private EditText locationView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
 
           setContentView(R.layout.activity_maps);
 
-//        location = (TextView) findViewById(R.id.location);
-
         MapFragment mapFragment  = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
-
-
 
     }
 
@@ -66,6 +66,8 @@ public class Home extends ActionBarActivity  implements OnMapReadyCallback {
         map.getUiSettings().setMyLocationButtonEnabled(false);
         boolean gps_enabled = false;
         boolean network_enabled = false;
+        map.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        map.setMyLocationEnabled(true);
 
         LatLng me;
         LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
@@ -73,12 +75,15 @@ public class Home extends ActionBarActivity  implements OnMapReadyCallback {
         gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
         network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
 
+        System.out.println("NETWORK IS "+network_enabled);
+        System.out.println("GPS IS "+gps_enabled);
+
         Location location = null;
-        if(gps_enabled)
+        if(gps_enabled && !network_enabled)
         {
             location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         }
-        else if(network_enabled)
+        else if(network_enabled && !gps_enabled)
         {
             location = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
         }
@@ -86,10 +91,12 @@ public class Home extends ActionBarActivity  implements OnMapReadyCallback {
         {
             // might as well use network to save data
             location = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
         }
         else
         {
             //ask to enable one of them
+
         }
 
 
@@ -100,12 +107,13 @@ public class Home extends ActionBarActivity  implements OnMapReadyCallback {
         }
         else
         {
+            System.out.println("LOCATION NOT DETECTED! :(");
             me = new LatLng(0, 0);
         }
 
+
         // normal, hybrid, terrain, satellite
-        map.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
-        map.setMyLocationEnabled(true);
+
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(me, 14));
 
 
@@ -118,33 +126,55 @@ public class Home extends ActionBarActivity  implements OnMapReadyCallback {
         Home.this.startActivity(intent);
 
     }
+
     public void pinpointMe(View view)
     {
+        // DRAG IN EDIT TEXT, USE TO GET COORDINATES, MOVE MAP
+        MapFragment mapFragment  = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        //mapFragment.getMapAsync(this);
+
+        GoogleMap map = mapFragment.getMap();
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(41.466099,-73.485646), 14));
+
         locationView = (EditText) findViewById(R.id.location_search);
         final String locationText = locationView.getText().toString();
         System.out.println(locationText);
 
-        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        LocationManager lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
-        LatLng me;
+        boolean gps_enabled = false;
+        boolean network_enabled = false;
 
-        if(location != null) {
-            double longitude = location.getLongitude();
-            double latitude = location.getLatitude();
-            me = new LatLng(latitude, longitude);
+        gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        System.out.println("NETWORK IS "+network_enabled);
+        System.out.println("GPS IS "+gps_enabled);
+
+        Location location = null;
+        if(gps_enabled && !network_enabled)
+        {
+            location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        }
+        else if(network_enabled && !gps_enabled)
+        {
+            location = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        }
+        else if(gps_enabled && network_enabled)
+        {
+            // might as well use network to save data
+            location = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+
         }
         else
         {
-            me = new LatLng(0, 0);
+            //ask to enable one of them
+
         }
 
-        System.out.println("WAT YOU WANT:"+me);
 
-        // normal, hybrid, terrain, satellite
-//        map.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
-//        map.setMyLocationEnabled(true);
-//        map.moveCamera(CameraUpdateFactory.newLatLngZoom(me, 14));
 
 
         LocationListener locationListener = new LocationListener() {
@@ -156,22 +186,22 @@ public class Home extends ActionBarActivity  implements OnMapReadyCallback {
             @Override
             // onlocationchanged needs to change, causing so many gps references. data + battery = rip
             public void onLocationChanged(Location location) {
-                System.out.println("LOCATION CHANGED!");
-
-//                double lat = location.getLatitude();
-//                double lng = location.getLongitude();
-                String locationText = locationView.getText().toString();
-                String result = "";
-
-                try {
-                    String test = "https://www.google.com/maps/place/" + locationText;
-                    //new WebRetrieval().execute(test);
-                    System.out.println("User Entered: "+test);
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    System.out.println("ERORRRRR");
-                }
+//                System.out.println("LOCATION CHANGED!");
+//
+////                double lat = location.getLatitude();
+////                double lng = location.getLongitude();
+//                String locationText = locationView.getText().toString();
+//                String result = "";
+//
+//                try {
+//                    String test = "https://www.google.com/maps/place/" + locationText;
+//                    //new WebRetrieval().execute(test);
+//                    System.out.println("User Entered: "+test);
+//
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                    System.out.println("ERORRRRR");
+//                }
 
             }
 
@@ -186,7 +216,8 @@ public class Home extends ActionBarActivity  implements OnMapReadyCallback {
             }
         };
         // requestLocationUpdates params: provider, mintime, mindistance, intent (which is locationListener)
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 600000, 500, locationListener);
+
+        //lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 600000, 500, locationListener);
         //locationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, locationListener);
 
 
@@ -235,13 +266,13 @@ public class Home extends ActionBarActivity  implements OnMapReadyCallback {
 
             String userLocation = output.toString();
 
-            System.out.println("THE USER LOCATION: "+userLocation);
+            System.out.println("THE USER LOCATION: " + userLocation);
 
 
         }
-//
-//
-//    }
+        
 }
+
+
 }
 
