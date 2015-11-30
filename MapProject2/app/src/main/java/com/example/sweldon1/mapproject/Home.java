cb762939fb8e01a306b6060e768500e45ccf1f7f
sweldon <stmydchannel@gmail.com>
2015-11-30 17:34:25 -0500

    Added map functionality / UI update

--- MapProject2/app/src/main/java/com/example/sweldon1/mapproject/Home.java
+++ MapProject2/app/src/main/java/com/example/sweldon1/mapproject/Home.java
@@ -1,6 +1,7 @@
 package com.example.sweldon1.mapproject;
 
 import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
@@ -12,6 +13,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -34,25 +36,77 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
 
-public class Home extends ActionBarActivity {
+public class Home extends ActionBarActivity  implements OnMapReadyCallback {
 
 
     private TextView location;
     private TextView body;
+    private EditText locationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.basic_layout);
+
           setContentView(R.layout.activity_maps);
 
 //        location = (TextView) findViewById(R.id.location);
-//        body = (TextView) findViewById(R.id.body);
-//
-//        body.setText("Select an option to display data!\n");
+
+        MapFragment mapFragment  = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
 
-   //     final Button button = (Button) findViewById(R.id.syncmap);
+
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+
+        map.getUiSettings().setMyLocationButtonEnabled(false);
+        boolean gps_enabled = false;
+        boolean network_enabled = false;
+
+        LatLng me;
+        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+        gps_enabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        network_enabled = lm.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+        Location location = null;
+        if(gps_enabled)
+        {
+            location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        }
+        else if(network_enabled)
+        {
+            location = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        }
+        else if(gps_enabled && network_enabled)
+        {
+            // might as well use network to save data
+            location = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        }
+        else
+        {
+            //ask to enable one of them
+        }
+
+
+        if(location != null) {
+            double longitude = location.getLongitude();
+            double latitude = location.getLatitude();
+            me = new LatLng(latitude, longitude);
+        }
+        else
+        {
+            me = new LatLng(0, 0);
+        }
+
+        // normal, hybrid, terrain, satellite
+        map.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        map.setMyLocationEnabled(true);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(me, 14));
 
 
     }
@@ -64,39 +118,80 @@ public class Home extends ActionBarActivity {
         Home.this.startActivity(intent);
 
     }
+    public void pinpointMe(View view)
+    {
+        locationView = (EditText) findViewById(R.id.location_search);
+        final String locationText = locationView.getText().toString();
+        System.out.println(locationText);
+
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        LatLng me;
+
+        if(location != null) {
+            double longitude = location.getLongitude();
+            double latitude = location.getLatitude();
+            me = new LatLng(latitude, longitude);
+        }
+        else
+        {
+            me = new LatLng(0, 0);
+        }
+
+        System.out.println("WAT YOU WANT:"+me);
+
+        // normal, hybrid, terrain, satellite
+//        map.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+//        map.setMyLocationEnabled(true);
+//        map.moveCamera(CameraUpdateFactory.newLatLngZoom(me, 14));
 
 
-//    public void findme(View view) {
-//        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-//        LocationListener locationListener = new LocationListener() {
-//            public void onLocationChanged(Location location) {
+        LocationListener locationListener = new LocationListener() {
+            public void onProviderEnabled(String provider) {
+
+
+            }
+
+            @Override
+            // onlocationchanged needs to change, causing so many gps references. data + battery = rip
+            public void onLocationChanged(Location location) {
+                System.out.println("LOCATION CHANGED!");
+
 //                double lat = location.getLatitude();
 //                double lng = location.getLongitude();
-//                String result = "";
-//
-//                try {
-//                    String[] test = {"http://data.fcc.gov/api/block/2010/find?latitude=" + lat + "&longitude=" + lng, "http://alerts.weather.gov/cap/us.php?x=1"};
-//                    new WebRetrieval().execute(test);
-//
-//                } catch (Exception e) {
-//                    e.printStackTrace();
-//                    System.out.println("ERORRRRR");
-//                }
-//            }
-//
-//            public void onStatusChanged(String provider, int status, Bundle extras) {
-//            }
-//
+                String locationText = locationView.getText().toString();
+                String result = "";
+
+                try {
+                    String test = "https://www.google.com/maps/place/" + locationText;
+                    //new WebRetrieval().execute(test);
+                    System.out.println("User Entered: "+test);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    System.out.println("ERORRRRR");
+                }
+
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
 //            public void onProviderEnabled(String provider) {
 //            }
-//
-//            public void onProviderDisabled(String provider) {
-//            }
-//        };
-//        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-//
-//
-//    }
+
+            public void onProviderDisabled(String provider) {
+            }
+        };
+        // requestLocationUpdates params: provider, mintime, mindistance, intent (which is locationListener)
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 600000, 500, locationListener);
+        //locationManager.requestSingleUpdate(LocationManager.GPS_PROVIDER, locationListener);
+
+
+    }
+
 
     //Starting new activity
 //    public void plotme(View view) {
@@ -104,80 +199,49 @@ public class Home extends ActionBarActivity {
 //        Home.this.startActivity(intent);
 //    }
 //
-//    private class WebRetrieval extends AsyncTask<String, Void, ArrayList> {
+    private class WebRetrieval extends AsyncTask<String, Void, String> {
+
+        private Exception exception;
+        String result = "";
+
+        @Override
+        protected String doInBackground(String... inputURL) {
+            try {
+
+                URL oracle = new URL(inputURL[0]);
+                URLConnection yc = oracle.openConnection();
+                BufferedReader in = new BufferedReader(new InputStreamReader(
+                        yc.getInputStream()));
+                String inputLine;
+                while ((inputLine = in.readLine()) != null)
+                    //System.out.println(inputLine);
+                    result += inputLine;
+                in.close();
+
+
+                System.out.println(result);
+
+            } catch (Exception e) {
+                this.exception = e;
+                return null;
+            }
+
+            return result;
+
+        }
 //
-//        private Exception exception;
-//
-//        ArrayList<String> output = new ArrayList<>();
-//        String result = "";
-//        String alertResult = "";
-//
-//        @Override
-//        protected ArrayList doInBackground(String... inputURL) {
-//            try {
-//
-//
-//
-//
-//                URL oracle = new URL(inputURL[0]);
-//                URLConnection yc = oracle.openConnection();
-//                BufferedReader in = new BufferedReader(new InputStreamReader(
-//                        yc.getInputStream()));
-//                String inputLine;
-//                while ((inputLine = in.readLine()) != null)
-//                    //System.out.println(inputLine);
-//                    result += inputLine;
-//                in.close();
-//                output.add(result);
-//
-//                URL alerts = new URL(inputURL[1]);
-//                URLConnection yd = alerts.openConnection();
-//                BufferedReader io = new BufferedReader(new InputStreamReader(
-//                        yd.getInputStream()));
-//                String alertLine;
-//                while ((alertLine = io.readLine()) != null)
-//                    //System.out.println(inputLine);
-//                    alertResult += alertLine;
-//                io.close();
-//                output.add(alertResult);
-//
-//
-//
-//
-//            } catch (Exception e) {
-//                this.exception = e;
-//                return null;
-//            }
-//
-//            return output;
-//
-//        }
-//
-//        @Override
-//        protected void onPostExecute(ArrayList output) {
-//
-//            String userLocation = output.get(0).toString();
-//
-//            String alertString = output.get(1).toString();
-//
-//
-//            org.jsoup.nodes.Document infoXML = Jsoup.parse(userLocation, "", Parser.xmlParser());
-//            org.jsoup.nodes.Document alertXML = Jsoup.parse(alertString, "", Parser.xmlParser());
-//            location.setText("General Location: "+infoXML.select("County").attr("name") + ", " + infoXML.select("State").attr("name"));
-//
-//            Elements elements = alertXML.select("id");
-//            body.setText("");
-//            for (Element element : elements) {
-//
-//                body.append(element.text()+"\n");
-//
-//
-//            }
-//
-//
-//        }
+        @Override
+        protected void onPostExecute(String output) {
+
+            String userLocation = output.toString();
+
+            System.out.println("THE USER LOCATION: "+userLocation);
+
+
+        }
 //
 //
 //    }
+}
 }
 
--- MapProject2/app/src/main/java/com/example/sweldon1/mapproject/MapsActivity.java
+++ MapProject2/app/src/main/java/com/example/sweldon1/mapproject/MapsActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -25,6 +26,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private TextView body;
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +36,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
 
         body = (TextView) findViewById(R.id.body);
-
 
         MapFragment mapFragment  = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
@@ -48,7 +51,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng me;
         LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
         //Location location = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-        Location location = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
         if(location != null) {
             double longitude = location.getLongitude();
@@ -61,12 +64,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
         // normal, hybrid, terrain, satellite
-       // map.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        map.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
         map.setMyLocationEnabled(true);
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(me, 14));
 
 
     }
+
+
 
 //    public void syncmap(View view) {
 //        //body.append("Sync map!");
